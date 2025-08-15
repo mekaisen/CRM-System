7817500dbc5a8da76bbe3bb1bdc938946c3b1bc0
Alexandr Hohlov <mekaisenfapsgun@gmail.com>
2025-08-15 10:00:48 +0500

    Доделал валидацию

--- src/helpers/utils/validateTitle.ts
+++ src/helpers/utils/validateTitle.ts
@@ -1,11 +1,12 @@
-export const validateTitle = (value: any) => {
-  if (value.length === 0) {
+export const validateTitle = (value: string) => {
+  const title = value.trim()
+  if (title.length === 0) {
     return 'Это поле не может быть пустым';
   }
-  if (value.length < 2 && value.length > 0) {
+  if (title.length < 2 && title.length > 0) {
     return 'Минимальная длина текста 2 символа';
   }
-  if (value.length > 64) {
+  if (title.length > 64) {
     return 'Вставить можно текст длиной меньше 64 символа';
   }
   return '';
