2e8675fb337006d61eb378319d772ac0f665e492
Alexandr Hohlov <mekaisenfapsgun@gmail.com>
2025-08-22 12:54:00 +0500

    feat: переделал layout на antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
-import { createBrowserRouter } from 'react-router';
+import type { GetProp, MenuProps } from 'antd';
+
+import { UnorderedListOutlined, UserOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import { Content } from 'antd/es/layout/layout';
+import Sider from 'antd/es/layout/Sider';
+import { useState } from 'react';
+import { createBrowserRouter, Link, Outlet } from 'react-router';
 
 import { TodosPage } from '@/pages/Todos/TodosPage.tsx';
 
 import './App.css';
 
+type MenuItem = GetProp<MenuProps, 'items'>[number];
+const items: MenuItem[] = [
+  {
+    key: '1',
+    icon: <UnorderedListOutlined />,
+    label: <Link to='/'>Todo</Link>
+  },
+  {
+    key: '2',
+    icon: <UserOutlined />,
+    label: <Link to='/profile'>Profile</Link>
+  }
+];
 const App = () => {
+  const [isOpen, setIsopen] = useState<boolean>(false);
+
   return (
     <>
-      <TodosPage />
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          breakpoint={'md'}
+          collapsed={isOpen}
+          collapsible
+          onCollapse={(value) => setIsopen(value)}
+        >
+          <Menu defaultSelectedKeys={['1']} items={items} theme='dark' mode='inline' />
+        </Sider>
+        <Layout>
+          <Content>
+            <Outlet />
+          </Content>
+        </Layout>
+      </Layout>
     </>
   );
 };
@@ -15,7 +51,15 @@ const App = () => {
 export const router = createBrowserRouter([
   {
     path: '/',
-    Component: App
+    Component: App,
+
+    children: [
+      { index: true, Component: TodosPage },
+      {
+        path: '/profile',
+        element: <div>Profile</div>
+      }
+    ]
   }
 ]);
 
