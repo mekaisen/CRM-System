a55fec87117c4d19c8f210f589768bc3ccede5f4
Aleksandr Hohlov <mekaiseninter@gmail.com>
2025-09-13 17:48:48 +0300

    fix: рефакторинг кода после ревью

--- src/store/utils.ts
+++ src/store/utils.ts
@@ -28,7 +28,7 @@ export const getAsyncRequestData = <T>(
   stateParam: IAsyncParticle<T>
 ): {
   data: T | null;
-  error: unknown;
+  error: string | null;
   status: IAsyncDataStatus;
 } => ({
   data: stateParam?.data,
