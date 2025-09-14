700889b53cb5fdf5cce30304dc127dd7536a4a48
Aleksandr Khokhlov <121090878+mekaisen@users.noreply.github.com>
2025-09-14 18:10:23 +0300

    Revert "Feature/ed 1457 users"

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -25,22 +25,22 @@ const initialState: IAuthStore = {
   authTokens: initAsyncParticle()
 };
 
-const errorsForSignin: Record<number, string> = {
+const errorsForSignin: Record<any, string> = {
   400: 'Ошибка десериализации запроса или неверный ввод',
   401: 'Неверные учетные данные',
   500: 'Внутренняя ошибка сервера'
 };
-const errorsForSignUp: Record<number, string> = {
+const errorsForSignUp: Record<any, string> = {
   400: 'Ошибка десериализации запроса или неверный ввод',
   409: 'Пользователь уже существует',
   500: 'Внутренняя ошибка сервера'
 };
-const errorsForRefresh: Record<number, string> = {
+const errorsForRefresh: Record<any, string> = {
   400: 'Ошибка десериализации запроса',
   401: 'Неверные учетные данные или токен истек',
   500: 'Внутренняя ошибка сервера'
 };
-const errorsForProfile: Record<number, string> = {
+const errorsForProfile: Record<any, string> = {
   400: 'Пользователь не найден',
   500: 'Внутренняя ошибка сервера'
 };
--- src/store/utils.ts
+++ src/store/utils.ts
@@ -28,7 +28,7 @@ export const getAsyncRequestData = <T>(
   stateParam: IAsyncParticle<T>
 ): {
   data: T | null;
-  error: string | null;
+  error: any;
   status: IAsyncDataStatus;
 } => ({
   data: stateParam?.data,
